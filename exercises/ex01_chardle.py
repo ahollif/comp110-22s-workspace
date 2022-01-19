f7fd3343e3a7e300880adedb7b556e22e6ed41f1
Adam Hollifield <ahollif@live.unc.edu>
2022-01-19 13:28:14 -0500

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,40 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730231193"
+
+magic_word: str = input("Enter a 5-character word: ")
+
+if len(magic_word) != 5:
+    print("Error: Word must contain 5 characters.")
+    exit()
+else: 
+    magic_word = magic_word.lower()
+
+magic_character: str = input("Enter a single character: ")
+
+if len(magic_character) != 1:
+    print("Error: Character must be a single character.")
+    exit()
+else: 
+    magic_character = magic_character.lower()
+
+# I have some Python background, so I know it'll be more efficient to use a for loop for the next step.
+# However, I understand the same thing could be accomplished using a series of if statements. 
+
+print("Searching for " + magic_character + " in " + magic_word)
+
+ticker = 0  # Increasing ticker that allows for iteration through magic word
+instances = 0  # Running counter for instances of magic character
+
+for char in magic_word:
+    if magic_word[ticker] == magic_character:
+        print(magic_character + " found at index " + str(ticker))
+        ticker += 1 
+        instances += 1
+    else:
+        ticker += 1  # Index position checked will increase regardless of magic character being found or not
+
+if instances > 0:
+    print(str(instances) + " instances of " + magic_character + " found in " + magic_word)
+else:
+    print("No instances of " + magic_character + " found in " + magic_word)
